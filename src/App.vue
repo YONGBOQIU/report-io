168e2eb16f86b425084adf45e90fd32c75597823
qiuyongbo <qiuyongbo@gosuncn.com>
2026-07-09 18:56:52 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+function test() {
+  a = b;
+  console.log('test')
+}
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
