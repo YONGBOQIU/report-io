6aeb2512ab7c4f1feb4390271316e1556eeea18f
qiuyongbo <qiuyongbo@gosuncn.com>
2026-07-10 09:27:52 +0800

    fix: 修正 GitHub Pages base 路径为 /report-io/

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/vue-project/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/report-io/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
